235d792504efbb279cb2cac0bd961efa31be3180
Andrew Neth <the0x539@gmail.com>
2025-01-27 22:57:04 -0600

    colorlib: refactor new oklab code

--- src/color.lua
+++ src/color.lua
@@ -83,75 +83,60 @@ local function XYZfromLAB(l, a, b)
 	return x*ref_X, y*ref_Y, z*ref_Z
 end
 
+-- TODO: rewrite above functions to use this instead of embedding the constants into the arithmetic
+local function mat_vec_mul(matrix, vector)
+    local result = {}
+    for i = 1, #matrix do
+	local v = 0
+	for j = 1, #vector do
+	    v = v + matrix[i][j] * vector[j]
+	end
+	result[i] = v
+    end
+    return result
+end
+
+local function vec_pow_inplace(vector, pow)
+	for i = 1, #vector do
+		vector[i] = math.pow(vector[i], pow)
+	end
+end
+
 local function OKLABfromXYZ(x, y, z)
-	local matrix1 = {
+	local lms_from_xyz = {
 	    {0.8189330101, 0.3618667424, -0.1288597137},
 	    {0.0329845436, 0.9293118715, 0.0361456387},
 	    {0.0482003018, 0.2643662691, 0.6338517070}
 	}
-	
-	local matrix2 = {
+
+	local lab_from_lms = {
 	    {0.2104542553, 0.7936177850, -0.0040720468},
 	    {1.9779984951, -2.4285922050, 0.4505937099},
 	    {0.0259040371, 0.7827717662, -0.8086757660}
 	}
-	
-	local xyz = {x, y, z}
-	
-	local function matrixVectorProduct(matrix, vector)
-	    local result = {}
-	    for i = 1, #matrix do
-	        result[i] = 0
-	        for j = 1, #vector do
-	            result[i] = result[i] + matrix[i][j] * vector[j]
-	        end
-	    end
-	    return result
-	end
-	
-	local lms = matrixVectorProduct(matrix1, xyz)
-	for i = 1, #lms do
-	    lms[i] = math.pow(lms[i], 1/3)
-	end
-	
-	local lab = matrixVectorProduct(matrix2, lms)
-	
+
+	local lms = mat_vec_mul(lms_from_xyz, {x, y, z})
+	vec_pow_inplace(lms, 1/3)
+	local lab = mat_vec_mul(matrix2, lms)	
 	return lab[1], lab[2], lab[3]
 end
 	
 local function XYZfromOKLAB(l, a, b)
-	local matrix1 = {
+	local xyz_from_lms = {
 	    {1.22701385, -0.55779998, 0.28125615},
 	    {-0.04058018, 1.11225687, -0.07167668},
 	    {-0.07638128, -0.42148198, 1.58616322}
 	}
-	
-	local matrix2 = {
+
+	local lms_from_lab = {
 	    {1, 0.39633779, 0.21580376},
 	    {1.00000001, -0.10556134, -0.06385417},
 	    {1.00000005, -0.08948418, -1.29148554}
 	}
 	
-	local lab = {l, a, b}
-	
-	local function matrixVectorProduct(matrix, vector)
-	    local result = {}
-	    for i = 1, #matrix do
-	        result[i] = 0
-	        for j = 1, #vector do
-	            result[i] = result[i] + matrix[i][j] * vector[j]
-	        end
-	    end
-	    return result
-	end
-	
-	local lms = matrixVectorProduct(matrix2, lab)
-	for i = 1, #lms do
-	    lms[i] = math.pow(lms[i], 3)
-	end
-	
-	local xyz = matrixVectorProduct(matrix1, lms)
-	
+	local lms = mat_vec_mul(lms_from_lab, {l, a, b})
+	vec_pow_inplace(lms, 3)
+	local xyz = mat_vec_mul(xyz_from_lms, lms)
 	return xyz[1], xyz[2], xyz[3]
 end
 	
